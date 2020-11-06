3e60483fb06b228c736f9060adb782ee9f5361c3
David Dugle <david.dugleii@yahoo.com>
2020-11-05 20:46:16 -0500

    got the timer to work and the clear of the screen to work

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ https://daviddugle.github.io/API-CodeQuiz/
 
 *set up initial files
 *pseudo coded the pages that will be used
+*got the first page welcome screen up
+*timer is working now and so is the display none for the welcome screen. (pretty proud of this)
--- script.js
+++ script.js
@@ -1,7 +1,59 @@
+var timerEl = document.querySelector("#timer");
+var welcomeEl = document.querySelector("#welcome-screen");
+var startEl = document.querySelector("#start-button");
+
+// need to have a button that starts the quiz and the timer. the start button should hide the welcome screen
+var timer = 60;
+
+
+startEl.addEventListener("click", function () {
+
+
+    welcomeEl.style.display = "none";
+    startClock();
+
+});
+
+
+function startClock() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+        timerEl.textContent = timeLeft;
+        timeLeft--;
+
+        if (timeLeft <= 0) {
+            timerEl.textContent = "";
+            clearInterval(timeInterval);
+        }
+
+
+    }, 1000);
+
+};
+
+
+// function prepareRead() {
+//     var timeLeft = 5;
+
+//     var timeInterval = setInterval(function() {
+//       timerEl.textContent = timeLeft + " seconds remaining";
+//       timeLeft--;
+
+//       if (timeLeft === 0) {
+//         timerEl.textContent = "";
+//         speedRead();
+//         clearInterval(timeInterval);
+//       }
+
+//     }, 1000);
+//   }
+
+
+
 // need to have an array of questions to ask for the quiz
 
 
-// need to have a button that starts the quiz and the timer. the start button should hide the welcome screen
+
 
 
 // need to set up the timer. timer should end the game at 0
